b7d33963d9542025a780ab7dff404af4292d81c4
Robert Nealan <rob@atomidesign.com>
2019-08-09 15:31:48 -0700

    feat(footer): Add React Footer (#586)
    * WIP - with kevin
    
    * Updating footer.
    
    * Shoehorns react footer into docs website – this repo definitely needs some TLC as its currently bandaids on bandaids keeping everything looking correct.
    
    * Revert livereload removal.

---
+++ assets/react/precache-manifest.a07f59ba54bbf1d518d7e35de4493603.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1cc6a4c22e7600ad8893eedf3ec229c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98d5d6b88aa2d1c78d78",
+    "url": "/static/js/main.af139ce3.js"
+  }
+]);
